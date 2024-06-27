e8c3a0ada83844a68f5a150d50451263e449af46
hosein khaleghi <hoseinkhaleghi97@gmail.com>
2024-06-27 16:18:22 +0330

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useContext, useState, useMemo } from "react";
+import AppContext from "./appContext";
+import Form from "./Components/Form";
+import Sidebar from "./Components/Sidebar";
+import ExtendedTree from "./Components/Tree";
+import { getNodes } from "./transportLayer";
+import { deleteEmptyChildren  } from "./Components/Util/utils";
+import { NodeType } from "./types";
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [showEdit, setShowEdit] = useState(true);
+  const [treeData, setTreeData] = useState([]);
+  const [selectedNodeId, setSelectedNodeId] = useState([]);
+  const [clipBoard, setClipBoard] = useState(null);
+
+
+
+  // const selectedClipboard = cutEmptyChildren
+
+  const selectedNodeIdString = selectedNodeId.join("");
+  // console.log(selectedNodeIdString);
+
+  const fetchTreeData = async () => {
+    const result = await getNodes();
+    setTreeData(result);
+  };
+
+  useEffect(() => {
+    fetchTreeData();
+  }, []);
+  // const handleCut = ()=>{setClipBoard(cutEmptyChildren)}
+
+  const handleContextMenuClick = (actionKey: any) => () => {
+    switch (actionKey) {
+      case "Add":
+        console.log("Add");
+        break;
+      case "Delete":
+        setTreeData((prevResult) => {
+          return deleteEmptyChildren(prevResult, selectedNodeIdString);
+        });
+        break;
+      case "Paste":
+        console.log("Paste");
+        // handleUpdateNode(selectedNodeKey, selectedNodeIdString);
+        break;
+      case "Cut":
+        // setClipBoard(cutEmptyChildren)
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        // (()=>{handleCut})
+        console.log(clipBoard);
+
+        break;
+      default:
+        console.log("not");
+    }
+  };
+  // const handleUpdateTree = (nodes: NodeType[]) => {};
+  const handleUpdateTree = (nodes: NodeType[]) => {
+    setTreeData(nodes);
+  };
+
+  const handleUpdateNode = (key: string, data: any) => {
+    // const updatedTreeData = [...treeData];
+    // اضافه کردن داده جدید به شاخه جدید
+    // حذف شاخه از جای قبلی
+    // setTreeData(updatedTreeData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider
+      value={{
+        treeData,
+        updateTreeData: handleUpdateTree,
+      }}
+    >
+      <div className="App">
+        <Sidebar>
+          <ExtendedTree
+            onSelect={setSelectedNodeId}
+            onCut={setClipBoard}
+            handleContextMenuClick={handleContextMenuClick}
+            // setSelectedNodeKey={setSelectedNodeKey}
+          />
+        </Sidebar>
+        {showEdit && <Form item={selectedItem} updateNode={handleUpdateNode}  />}
+      </div>
+    </AppContext.Provider>
   );
 }
-
 export default App;
