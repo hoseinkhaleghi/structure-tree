e82caaf0d454f81487d63a9186d77555b3490161
hosein khaleghi <hoseinkhaleghi97@gmail.com>
2024-06-29 06:31:05 +0330

    patch 03 table updated

--- src/Components/Table/index.tsx
+++ src/Components/Table/index.tsx
@@ -1,30 +1,48 @@
 import { Table } from "antd";
+import { useState } from "react";
 
 const AntTable = ({ initialValue }: any) => {
-  const columns = [
-    {
-      title: "Default",
-      dataIndex: "default",
-      key: "default",
-      render: (text: boolean) => <input type="checkbox" checked={text} />,
-    },
-    {
-      title: "Title",
-      dataIndex: "title",
-      key: "title",
-    },
-  ];
-
-  const dataSource = initialValue.map((node: any) => ({
-    default: node?.isDefault || false,
-    title: node?.title || "",
-  }));
-
-  return (
-    <div>
-      <Table columns={columns} dataSource={dataSource} />
-    </div>
-  );
-};
+	const [checkedValues, setCheckedValues] = useState(
+	  initialValue.map((node: any) => ({ value: node?.isDefault || false }))
+	);
+  
+	const columns = [
+	  {
+		title: "Default",
+		dataIndex: "default",
+		key: "default",
+		render: (text: boolean, record: any, recordIndex: number) => (
+		  <input
+			type="checkbox"
+			checked={checkedValues[recordIndex]?.value || false}
+			onChange={() => {
+			  const newCheckedValues = [...checkedValues];
+			  newCheckedValues[recordIndex] = {
+				...newCheckedValues[recordIndex],
+				value: !newCheckedValues[recordIndex]?.value || false
+			  };
+			  setCheckedValues(newCheckedValues);
+			}}
+		  />
+		),
+	  },
+	  {
+		title: "Title",
+		dataIndex: "title",
+		key: "title",
+	  },
+	];
+  
+	const dataSource = initialValue.map((node: any, index: number) => ({
+	  default: checkedValues[index]?.value || false,
+	  title: node?.title || "",
+	}));
+  
+	return (
+	  <div>
+		<Table columns={columns} dataSource={dataSource} />
+	  </div>
+	);
+  };
 
 export default AntTable;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ConfigProvider } from 'antd';
-// import 'antd/dist/antd.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(
