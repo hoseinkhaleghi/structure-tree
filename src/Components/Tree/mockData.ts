e3ba9ae00f22cc4bc5d39386718940beafedb17a
hoseinkhaleghi <hoseinkhaleghi97@gmail.com>
2024-10-06 13:19:48 +0330

    change name

--- src/Components/Tree/mockData.ts
+++ src/Components/Tree/mockData.ts
@@ -3,7 +3,7 @@ export function createTreeMockData(): NodeType[] {
   return [
     {
       key: "1",
-      title: "شرکت چارگون",
+      title: "شرکت",
       users: [],
       children: [
         {
